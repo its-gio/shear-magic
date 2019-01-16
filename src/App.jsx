7133e135c34c39d4a7751032c3f9db83a70e6bb4
its-gio <me@its-g.io>
2019-01-15 17:14:01 -0800

    Initial Setup

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { render } from "react-dom";
+import Layout from "./components/Layout.jsx";
+
+render(React.createElement(Layout), document.getElementById("root"));
