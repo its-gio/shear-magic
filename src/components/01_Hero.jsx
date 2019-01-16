f9f478b4340213d8c1fa46da89a24d12f9c930ab
its-gio <me@its-g.io>
2019-01-15 21:18:20 -0800

    Content in hero section added (Styles needed)

--- src/components/01_Hero.jsx
+++ src/components/01_Hero.jsx
@@ -4,9 +4,15 @@ class Hero extends Component {
   render() {
     return (
       <div className="hero">
-        <h1 className="hero--headline">
-          Alpaca, Llama, &amp; Sheep Shearing Services
-        </h1>
+        <div className="hero--content">
+          <h1 className="hero--content__headline">
+            Alpaca, Llama, &amp; Sheep Shearing Services
+          </h1>
+          <p className="hero--content__claimer">Shearing since 2013</p>
+          <button className="hero--content__contact">
+            Make An Appointment
+          </button>
+        </div>
       </div>
     );
   }
