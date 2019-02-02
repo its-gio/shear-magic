2afcccbce2501e99908318903c115ed7764941a5
its-gio <me@its-g.io>
2019-02-01 16:44:52 -0800

    Pricing component added to site

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Header from "./components/00_Header.jsx";
 import Hero from "./components/01_Hero.jsx";
 import About from "./components/02_About.jsx";
 import Shearing from "./components/03_Shearing.jsx";
+import Pricing from "./components/04_Pricing.jsx";
+
 
 class App extends Component {
   render() {
@@ -15,6 +17,7 @@ class App extends Component {
         <div className="divider shearing-div" />
         <Shearing />
         <div className="divider pricing-div" />
+        <Pricing />
       </React.Fragment>
     );
   }
---
+++ src/components/04_Pricing.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Pricing extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Pricing Schedule:</h2>
+      </div>
+    );
+  }
+}
+
+export default Pricing;
