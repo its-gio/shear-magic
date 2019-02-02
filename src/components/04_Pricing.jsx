9f4624df0eb8032d641afeb88216efc290417446
its-gio <me@its-g.io>
2019-02-01 16:56:34 -0800

    Adding content to pricing component

--- src/components/04_Pricing.jsx
+++ src/components/04_Pricing.jsx
@@ -3,8 +3,23 @@ import React, { Component } from "react";
 class Pricing extends Component {
   render() {
     return (
-      <div>
-        <h2>Pricing Schedule:</h2>
+      <div className="pricing">
+        <h2 className="pricing--title">Pricing Schedule:</h2>
+        <div className="pricing--content-container">
+          <ul className="pricing--content-container__list">
+            <li>$25 per alpaca</li>
+            <li>$45 per llama</li>
+            <li>$10 per sheep</li>
+            <li>$5 per animal nail trimming (alpaca and sheep)</li>
+            <li>$10 nail trimming per llama</li>
+            <li>$50 Ranch Call</li>
+          </ul>
+          <p className="pricing--content-container__request">Please ensure you are prepared on the day of your appointment. Those details can be found  in the shearing tab above. If you are not prepared when Sergio arrives, at his discretion he may cancel or reschedule the appointment.</p>
+        </div>
+        <blockquote className="pricing--blockquote">
+            <p>Note:</p>
+            <p>If contact and establishing an appointment is made after May 1st ranch call will be $100</p>
+          </blockquote>
       </div>
     );
   }
